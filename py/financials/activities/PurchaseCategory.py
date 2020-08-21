3527ea3a75f79d941d1f655d1bfeede96ca86198
daniel1596 <dmorton@my.milligan.edu>
2020-08-21 06:56:06 -0400

    Starting database and client layer over it. Exciting!

--- py/financials/activities/PurchaseCategory.py
+++ py/financials/activities/PurchaseCategory.py
@@ -2,8 +2,13 @@ from enum import Enum, auto
 
 
 class PurchaseCategory(Enum):
+    # could combine gas, car other into "vehicle maintenance"...
+    # that's what Citi does, but I'm not so sure.
+    # However, I feel like it would be a strength, not a weakness,
+    # To have more categories rather than fewer
+    
     BOOKS = auto()
-    CAR_MAINTENANCE = auto()  # separate from gas?
+    CAR_MAINTENANCE = auto()
     # CERTIFICATE_OF_DEPOSIT = auto()
     CLOTHING = auto()
     DINING = auto()
@@ -15,8 +20,9 @@ class PurchaseCategory(Enum):
     GROCERY = auto()
     GROOMING_HYGIENE = auto()
     HOUSEHOLD_SUPPLIES = auto()
-    HOUSING = auto()
+    HOUSING = auto() # does this include lodging e.g. hotels?
     MEDICAL = auto()
     MISC = auto()
+    TRAVEL = auto() # or do hotels go in this category? hmm.    
     UTILITIES = auto()
     # STOCK = auto() #? I'm not sure about this one. This is more of a transfer of assets from one type to another.
---
+++ sql/ResetData.sql
@@ -0,0 +1,5 @@
+-- This would be useful if I was sharing the application with Dad
+-- Note that there is no "truncate table" in SQLite.
+-- See https://www.techonthenet.com/sqlite/truncate.php
+
+DELETE FROM FinancialActivity
