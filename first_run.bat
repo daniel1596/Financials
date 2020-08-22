a71c7fb5e47e1ccea6512f2e3dc633b783dae53b
daniel1596 <dmorton@my.milligan.edu>
2020-08-22 10:54:58 -0400

    Starting to build page for Excel importing and table of transactions so far (probably editable)

--- first_run.bat
+++ first_run.bat
@@ -3,6 +3,8 @@ echo.
 echo Tasks for this file would include:
 echo - Calling a (Python?) script to clear all transaction data from the db (just in case), etc.
 echo - Installing all Python packages via a requirements.txt file (to be done later)
+:: pip install -r requirements.txt
+
 echo - Maybe some sort of readme popping up on the screen? Not sure if that would be needed.
 echo (Note - don't need to install npm. Vue is included via script reference.)
 echo (SQLite doesn't need to be installed either. Python has it in the base library.)
